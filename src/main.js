7a14f1c27f837889303d31a37f3df67946c73e85
GermanSTr <gershev1994@gmail.com>
2023-11-24 17:21:08 +0200

    select-fix

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+import '../node_modules/slim-select/dist/slimselect.css';
 
+import SlimSelect from 'slim-select';
+
+new SlimSelect({
+  select: '.home_categorias-search',
+  settings: {
+    placeholderText: 'Categories',
+    showSearch: false,
+    searchHighlight: true,
+  },
+});
+
+new SlimSelect({
+  select: '.home_categorias-sort',
+  settings: {
+    placeholderText: 'A to Z',
+    showSearch: false,
+    searchHighlight: true,
+  },
+});
