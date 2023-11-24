7ec265de24f78a78158721c0cf3e6550ad19358b
BoikoAlona <a.semikhova@gmail.com>
2023-11-24 17:32:15 +0200

    create murkupProductsCards.js

---
+++ src/js/markup/createMarkupProductsCard.js
@@ -0,0 +1,28 @@
+export function createMarkupProducts(data) {
+    const markup = data
+        .map(({ img, name, category, size, popularity, price }) => {
+            return `<div class="product-card">
+                <div class="image-product">
+                    <img class="image" src="${img}" alt="${name}" />
+                </div>
+                <div class="info-product">
+                    <h3 class="name-product">${name}</h3>
+                    <p class="characteristics-product">
+                        Category:<span class="details-product">${category.replaceAll('_', ' ')}</span> Size:<span
+                            class="details-product">${size}</span>
+                        Popularity:<span class="details-product">${popularity}</span>
+                    </p>
+                </div>
+                <div class="price">
+                    <p class="price-product">${price}</p>
+                    <button class="button-shopping">
+                    <svg class="icon-shopping" width="28" height="28">
+                        <use href="../images/icons/icons.svg#icon-shopping-cart"></use>
+                    </svg>
+                    </button>
+                </div>
+            </div>`;
+        })
+        .join('');
+    return markup;
+}
