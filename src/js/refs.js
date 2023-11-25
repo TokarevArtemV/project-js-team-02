b9a776bae10a6e5ad270dd12ec77f9925d66045f
imlaamer <lolitayenik@ukr.net>
2023-11-26 01:14:01 +0200

    fix footer and other partials

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,1 +1,10 @@
-export const refs = {};
+export const refs = {
+  //footer
+  footerSubmitBtnEl: document.querySelector('.footer-form-btn'),
+  footerInputEl: document.querySelector('.footer-input'),
+  footerBackdropEl: document.querySelector('[data-modal-footer]'),
+  footerModalEl: document.querySelector('.footer-modal'),
+  footerModalBtnCloseEl: document.querySelector('[ data-modal-close]'),
+  footerFormEl: document.querySelector('.footer-form'),
+  //
+};
