96eb7f78d95039272ddabfeee01ae5f91a89c484
GermanSTr <gershev1994@gmail.com>
2023-11-26 19:52:26 +0200

    markup

--- src/js/markup/createMarkupBasketProductsCard.js
+++ src/js/markup/createMarkupBasketProductsCard.js
@@ -3,25 +3,28 @@ import icon from '../../images/icons/icons.svg';
 export function createMarkupBasketProductsCard(data) {
   const markup = data
     .map(({ img, name, category, price, size }) => {
-      return `<div class="product-cart">
-  
-  <img src="${img}" alt="${name}" class="product-image" />
-  
-  <div class="product-details">
-    <h2 class="product-name">${name}</h2>
-    <div class="cart-product-box">
-      <p class="product-category">
-    <span class="category-slyle">${category.replaceAll('_', ' ')}</span>
-      </p>
-      <p class="product-size">
-        <span class="size-slyle">${size}</span>
-      </p>
-    </div>
-    <p class="product-price">${price}</p>
-
-  </div>
-</div>
-`;
+      return `<div class="product-cart-js">
+          <div class="basket-img-box">
+            <img src="${img}" alt="${name}" class="product-image" />
+          </div>
+          <div class="product-details">
+            <button class="remove-button">
+              <svg class="icon-close" width="18" height="18">
+                <use href="./images/icons/icons.svg#icon-close-sharp"></use>
+              </svg>
+            </button>
+            <h2 class="product-name">${name}</h2>
+            <div class="cart-product-box">
+              <p class="product-basket-text">
+                Category: <span class="category-slyle">${category}</span>
+              </p>
+              <p class="product-basket-text">
+                Size: <span class="size-slyle">${size}</span>
+              </p>
+            </div>
+            <p class="product-price">${price}</p>
+          </div>
+        </div>`;
     })
     .join('');
 
