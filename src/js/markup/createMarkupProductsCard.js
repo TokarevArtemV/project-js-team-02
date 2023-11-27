69de5576eb4d58a4ad205096975009bea8b09ae5
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-11-27 13:27:05 +0200

    add scroll discount

--- src/js/markup/createMarkupProductsCard.js
+++ src/js/markup/createMarkupProductsCard.js
@@ -1,17 +1,33 @@
 import icon from '../../images/icons/icons.svg';
 
 export function createMarkupProducts(data) {
-
-    const markup = data
-        .map(({ img, name, category, size, popularity, price, _id }) => {
-            return `<li data-id="${_id}" class="product-card js-product-card">
+  const markup = data
+    .map(
+      ({
+        img,
+        name,
+        category,
+        size,
+        popularity,
+        price,
+        _id,
+        is10PercentOff,
+      }) => {
+        if (is10PercentOff) {
+          return `<li data-id="${_id}" class="product-card js-product-card">
                 <div class="image-product">
+                <svg class="icon-discount" width="60" height="60">
+              <use href="${icon}#icon-discount"></use>
+            </svg>
                     <img class="image" src="${img}" alt="${name}" />
                 </div>
                 <div class="info-product">
                     <h3 class="name-product">${name}</h3>
                     <p class="characteristics-product">
-                        Category:<span class="details-product">${category.replaceAll('_',  ' ')}</span> Size: <span
+                        Category:<span class="details-product">${category.replaceAll(
+                          '_',
+                          ' '
+                        )}</span> Size: <span
                             class="details-product">${size}</span>
                         Popularity: <span class="details-product">${popularity}</span>
                     </p>
@@ -25,7 +41,34 @@ export function createMarkupProducts(data) {
                     </button>
                 </div>
             </li>`;
-        })
-        .join('');
-    return markup;
-    }
+        } else {
+          return `<li data-id="${_id}" class="product-card js-product-card">
+                <div class="image-product">
+                    <img class="image" src="${img}" alt="${name}" />
+                </div>
+                <div class="info-product">
+                    <h3 class="name-product">${name}</h3>
+                    <p class="characteristics-product">
+                        Category:<span class="details-product">${category.replaceAll(
+                          '_',
+                          ' '
+                        )}</span> Size: <span
+                            class="details-product">${size}</span>
+                        Popularity: <span class="details-product">${popularity}</span>
+                    </p>
+                </div>
+                <div class="product-card-price">
+                    <p class="price-product">${price}</p>
+                    <button class="js-button-shopping">
+                    <svg class="icon-shopping-card" width="28" height="28">
+                        <use href="${icon}#icon-shopping-cart"></use>
+                    </svg>
+                    </button>
+                </div>
+            </li>`;
+        }
+      }
+    )
+    .join('');
+  return markup;
+}
