efa909a8e4f3880b283547e6b168f31f8a094c97
GermanSTr <gershev1994@gmail.com>
2023-11-29 00:38:44 +0200

    deleteProduct

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,9 @@ import { renderBasketProducts } from './renderBasketProducts';
 import { validateInput } from './validateInput';
 import { refs } from './refs';
 import { countCartProducts } from './cartCount';
+import './deleteBasketProductCard';
+import { deleteBasketProductCards } from './deleteBasketProductCard';
+import throttle from 'lodash.throttle';
 
 // відмалювання товарів в корзині
 renderBasketProducts();
@@ -13,3 +16,9 @@ countCartProducts();
 
 //валідація форми
 validateInput(refs.inputCartEl, refs.submitBtnCartEl);
+
+//закриття продуктових карток натисканням на кнопку
+refs.productFormBasket.addEventListener(
+  'click',
+  throttle(deleteBasketProductCards, 1000)
+);
