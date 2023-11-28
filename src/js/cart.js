189ec35fe097b2aa347f5a4027f856114e3d2a99
imlaamer <lolitayenik@ukr.net>
2023-11-29 01:16:13 +0200

    cart-css-js

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,7 @@ import { renderBasketProducts } from './renderBasketProducts';
 import { validateInput } from './validateInput';
 import { refs } from './refs';
 import { countCartProducts } from './cartCount';
+import { cartItemCounter } from './cartItemCounter';
 
 // відмалювання товарів в корзині
 renderBasketProducts();
@@ -13,3 +14,6 @@ countCartProducts();
 
 //валідація форми
 validateInput(refs.inputCartEl, refs.submitBtnCartEl);
+
+//лічильник
+cartItemCounter();
