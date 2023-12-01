e2543eed962b49392746bb88a3ab96bbe3b65a9d
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-12-01 03:25:22 +0200

    start

---
+++ node_modules/slim-select/src/vue/dist/slim-select/settings.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="node" />
+export type SettingsPartial = Partial<Settings>;
+export default class Settings {
+    id: string;
+    style: string;
+    class: string[];
+    isMultiple: boolean;
+    isOpen: boolean;
+    isFullOpen: boolean;
+    intervalMove: NodeJS.Timeout | null;
+    disabled: boolean;
+    alwaysOpen: boolean;
+    showSearch: boolean;
+    searchPlaceholder: string;
+    searchText: string;
+    searchingText: string;
+    searchHighlight: boolean;
+    closeOnSelect: boolean;
+    contentLocation: HTMLElement;
+    contentPosition: 'relative' | 'absolute';
+    openPosition: 'auto' | 'up' | 'down';
+    placeholderText: string;
+    allowDeselect: boolean;
+    hideSelected: boolean;
+    showOptionTooltips: boolean;
+    minSelected: number;
+    maxSelected: number;
+    timeoutDelay: number;
+    maxValuesShown: number;
+    maxValuesMessage: string;
+    constructor(settings?: SettingsPartial);
+}
